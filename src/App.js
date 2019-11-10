00f2e696c93c2e64bf30973c7b6c87e87f1259c4
Julian Konchunas <konchunas@gmail.com>
2019-11-10 19:14:49 +0200

    Add oo7; Connect to substrate node

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React from "react";
 import { Stage, Container, AppConsumer, Graphics } from "@inlet/react-pixi";
 import Viewport from "./components/Viewport";
 import Chunk from "./components/Chunk";
+import Heading from "./components/Heading";
+import { Segment, Header, Rail, Label, Menu, Grid } from "semantic-ui-react";
+import { Bond, TransformBond, AddCodecTransform } from "oo7";
+import { ReactiveComponent, If, Rspan } from "oo7-react";
 import {
-  Segment,
-  Header,
-  Card,
-  Sticky,
-  Rail,
-  Label,
-  Menu,
-  Grid,
-} from "semantic-ui-react";
+  calls,
+  runtime,
+  chain,
+  system,
+  runtimeUp,
+  ss58Decode,
+  ss58Encode,
+  pretty,
+  addressBook,
+  secretStore,
+  metadata,
+  nodeService,
+  bytesToHex,
+  hexToBytes,
+  AccountId
+} from "oo7-substrate";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 const { createRef, useState } = React;
@@ -20,68 +33,102 @@ const CHUNKS_PER_SIDE = 8;
 const CHUNK_SIDE = 8;
 const FIELD_SIZE = CHUNKS_PER_SIDE * CHUNK_SIDE;
 
-// app
-const App = () => {
-  const contextRef = createRef();
+export class App extends ReactiveComponent {
+  constructor() {
+    super([], { ensureRuntime: runtimeUp });
 
-  let [selectedPixel, setSelectedPixel] = useState({});
+    // For debug only.
+    window.runtime = runtime;
+    window.secretStore = secretStore;
+    window.addressBook = addressBook;
+    window.chain = chain;
+    window.calls = calls;
+    window.system = system;
+    window.that = this;
+    window.metadata = metadata;
 
-  let onPixelSelected = (x, y, color) => {
-    // selectedPixel.x = x
-    // selectedPixel.y = y
-    // selectedPixel.color = color
-    setSelectedPixel({ x: x, y: y, color: color });
-  };
+    this.state = {
+      selectedPixel: {
+        color: "white",
+        x: 0,
+        y: 0
+      },
+      chunks: []
+    };
 
-  let chunks = [];
-  for (let i = 0; i < CHUNKS_PER_SIDE; i++) {
-    for (let j = 0; j < CHUNKS_PER_SIDE; j++) {
-      const key = `${i} ${j}`;
-      chunks.push(
-        <Chunk
-          key={key}
-          x={i * 8}
-          y={j * 8}
-          side={CHUNK_SIDE}
-          onPixelSelected={onPixelSelected}
-        />
-      );
-    }
+    // addCodecTransform('Kitty<Hash,Balance>', {
+    //     id: 'Hash',
+    //     dna: 'Hash',
+    //     price: 'Balance',
+    //     gen: 'u64'
+    // });
+
+    const contextRef = createRef();
   }
 
-  return (
-    <Segment>
-      <Segment ref={contextRef}></Segment>
-      <Stage width={720} height={720} options={{ backgroundColor: 0xbbbbbb }}>
-        <Container sortableChildren={true}>
-          <AppConsumer>
-            {app => (
-              <Viewport
-                app={app}
-                worldWidth={FIELD_SIZE}
-                worldHeight={FIELD_SIZE}
-              >
-                {chunks}
-              </Viewport>
-            )}
-          </AppConsumer>
-        </Container>
-      </Stage>
-      <Rail attached internal position="right">
-        <Segment>
-          <Header>Selected pixel</Header>
-          <div>
-            Color: <Label color={selectedPixel.color} />
-          </div>
-          <div>
-            Position: ({selectedPixel.x},{selectedPixel.y})
-          </div>
-          <div>Price: {selectedPixel.price}</div>
-        </Segment>
-      </Rail>
-    </Segment>
-  );
-};
+  setSelectedPixel = (pixel) => {
+    this.setState({
+      selectedPixel: pixel
+    });
+  }
+
+  onPixelSelected = (x, y, color) => {
+    this.setSelectedPixel({ x: x, y: y, color: color });
+  }
+
+  readyRender() {
+    let chunks = [];
+    for (let i = 0; i < CHUNKS_PER_SIDE; i++) {
+      for (let j = 0; j < CHUNKS_PER_SIDE; j++) {
+        const key = `${i} ${j}`;
+        chunks.push(
+          <Chunk
+            key={key}
+            x={i * 8}
+            y={j * 8}
+            side={CHUNK_SIDE}
+            onPixelSelected={this.onPixelSelected}
+          />
+        );
+      }
+    }
+
+    return (
+      <Segment>
+        <Segment></Segment>
+        <Stage width={720} height={720} options={{ backgroundColor: 0xbbbbbb }}>
+          <Container sortableChildren={true}>
+            <AppConsumer>
+              {app => (
+                <Viewport
+                  app={app}
+                  worldWidth={FIELD_SIZE}
+                  worldHeight={FIELD_SIZE}
+                >
+                  {chunks}
+                </Viewport>
+              )}
+            </AppConsumer>
+          </Container>
+        </Stage>
+        <Rail attached internal position="right">
+          <Segment>
+            <Header>Selected pixel</Header>
+            <div>
+              Color: <Label color={this.state.selectedPixel.color || "black"} />
+            </div>
+            <div>
+              Position: ({this.state.selectedPixel.x},
+              {this.state.selectedPixel.y})
+            </div>
+            <div>Price: {this.state.selectedPixel.price}</div>
+          </Segment>
+        </Rail>
+        <Heading></Heading>
+      </Segment>
+    );
+  }
+}
 
 // window.addEventListener("resize", resize);
 
---
+++ src/components/Heading.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Segment, Header, Rail, Label, Menu, Grid } from "semantic-ui-react";
+import { ReactiveComponent, If, Rspan } from "oo7-react";
+import { runtime, chain, system, nodeService, bytesToHex } from "oo7-substrate";
+
+import { Pretty } from "./Pretty";
+
+class Heading extends React.Component {
+  render() {
+    return (
+      <div>
+        <If
+          condition={nodeService().status.map(x => !!x.connected)}
+          then={
+            <Label>
+              Connected{" "}
+              <Label.Detail>
+                <Pretty
+                  className="value"
+                  value={nodeService().status.sub("connected")}
+                />
+              </Label.Detail>
+            </Label>
+          }
+          else={<Label>Not connected</Label>}
+        />
+        <Label>
+          Name{" "}
+          <Label.Detail>
+            <Pretty className="value" value={system.name} /> v
+            <Pretty className="value" value={system.version} />
+          </Label.Detail>
+        </Label>
+        <Label>
+          Chain{" "}
+          <Label.Detail>
+            <Pretty className="value" value={system.chain} />
+          </Label.Detail>
+        </Label>
+        <Label>
+          Runtime{" "}
+          <Label.Detail>
+            <Pretty className="value" value={runtime.version.specName} /> v
+            <Pretty className="value" value={runtime.version.specVersion} /> (
+            <Pretty className="value" value={runtime.version.implName} /> v
+            <Pretty className="value" value={runtime.version.implVersion} />)
+          </Label.Detail>
+        </Label>
+        <Label>
+          Height{" "}
+          <Label.Detail>
+            <Pretty className="value" value={chain.height} /> (with{" "}
+            <Pretty className="value" value={chain.lag} /> lag)
+          </Label.Detail>
+        </Label>
+        <Label>
+          Total issuance{" "}
+          <Label.Detail>
+            <Pretty className="value" value={runtime.balances.totalIssuance} />
+          </Label.Detail>
+        </Label>
+      </div>
+    );
+  }
+}
+
+export default Heading;
