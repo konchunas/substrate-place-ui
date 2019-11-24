5ee2a6f94396efc71a8e59211faaf29d9ae3c69b
Julian Konchunas <konchunas@gmail.com>
2019-11-25 00:37:58 +0200

    Separate pixel storing and rendering logic

--- src/components/Chunk.js
+++ src/components/Chunk.js
@@ -1,47 +1,64 @@
 import React from "react";
-import { Graphics, Container } from "@inlet/react-pixi";
+import { Container } from "@inlet/react-pixi";
 import Pixel from "./Pixel";
 import { utils } from "pixi.js";
 import { runtime } from "oo7-substrate"
 import { indexToCartesian } from "../utils"
-
-const { useState } = React;
+import { CHUNK_SIDE } from "../settings"
 
 const Chunk = props => {
-  let onClick = (x, y, color) => {
+  let onClick = React.useCallback((x, y, color) => {
     console.log(x, y, color);
     const htmlColor = utils.hex2string(color)
     props.onPixelSelected(x, y, htmlColor);
-  };
-  
+  }, [props])
+
+  let [pixels, setPixels] = React.useState(() => createMatrix(CHUNK_SIDE))
   let convertToCartesianChunk = (array) => {
+    let newPixels = createMatrix(CHUNK_SIDE)
     for (let i = 0; i < array.length; i++) {
       const {x, y} = indexToCartesian(i);
       const color = array[i].color
-      console.log(color.r)
+      const hexColor = utils.rgb2hex([color.r / 255.0, color.g / 255.0, color.b / 255.0])
+      newPixels[x][y] = hexColor
     }
-  }
+    setPixels(newPixels)
+  };
   
+  React.useEffect(() => {
+    let runtimePixels = runtime.place.chunks(props.chunkNumber)
+    runtimePixels.tie(convertToCartesianChunk)
+  }, []);
 
-  let runtimePixels = runtime.place.chunks(props.chunkNumber)
-  // runtimePixels.then((arg) => console.log(arg[0]))
-  runtimePixels.then(convertToCartesianChunk)
-
-  let pixels = [];
-  for (let i = 0; i < props.side; i++) {
-    for (let j = 0; j < props.side; j++) {
-      const key = `${i} ${j}`;
-      pixels.push(
-        <Pixel key={key} x={i} y={j} color={0xff0066} onClick={onClick} />
-      );
+  let getPixelComponents = React.useMemo(() => {
+    let pixelComponents = []
+    for (let i = 0; i < CHUNK_SIDE; i++) {
+      for (let j = 0; j < CHUNK_SIDE; j++) {
+        pixelComponents.push(
+            <Pixel key={`${i}${j}`} x={i} y={j} color={pixels[i][j]} onClick={onClick} />
+        );
+      }
     }
-  }
+    return pixelComponents
+  }, [pixels, onClick]);
 
   return (
     <Container {...props} interactive={true}>
-      {pixels}
+      {getPixelComponents}
     </Container>
   );
 };
+
+function createMatrix(side) {
+  let matrix = []
+  for (let i = 0; i < side; i++) {
+    let row = []
+    for (let j = 0; j < side; j++) {
+      row.push(0)
+    }
+    matrix.push(row)
+  }
+  return matrix
+}
 
 export default Chunk;
