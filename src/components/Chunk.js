87941f57ee892f96f1536965dc688ac05eeb1da7
Julian Konchunas <konchunas@gmail.com>
2019-12-04 23:15:12 +0200

    Chunk drawing optimization and memoization

--- src/components/Chunk.js
+++ src/components/Chunk.js
@@ -7,11 +7,14 @@ import { Graphics } from "@inlet/react-pixi";
 
 const DEBUG_DRAW = true;
 
-const Chunk = props => {
+const Chunk = React.memo(props => {
 
-  let [pixels, setPixels] = React.useState(() => createMatrix(PIXELS_PER_CHUNK))
+  let [pixels, setPixels] = React.useState([])
 
   let convertToCartesianChunk = (array) => {
+    if (array.length <= 0) {
+      return
+    }
     let newPixels = createMatrix(PIXELS_PER_CHUNK)
     for (let i = 0; i < array.length; i++) {
       const {x, y} = indexToCartesian(i, PIXEL_COORDS);
@@ -25,7 +28,7 @@ const Chunk = props => {
   React.useEffect(() => {
     let runtimePixels = runtime.place.chunks([props.chunkX, props.chunkY])
     runtimePixels.tie(convertToCartesianChunk)
-  }, []);
+  }, [props.chunkX, props.chunkY]);
 
 
   const instance = React.useRef(null);
@@ -33,13 +36,12 @@ const Chunk = props => {
     const pixel = event.data.getLocalPosition(instance.current);
     const x = Math.floor(pixel.x)
     const y = Math.floor(pixel.y)
-    const color = pixels[x][y]
     const globalX = x + props.chunkX * PIXELS_PER_CHUNK
     const globalY = y + props.chunkY * PIXELS_PER_CHUNK
-    const htmlColor = utils.hex2string(color)
+    const htmlColor = pixels.length > 0 ? utils.hex2string(pixels[x][y]) : "white"
     props.onPixelSelected(globalX, globalY, htmlColor)
   }
-  
+
   return (
     <Graphics
       ref={instance}
@@ -48,31 +50,28 @@ const Chunk = props => {
       click={onPixelClick}
       draw={g => {
         g.clear()
-        console.log(g.scale)
-        console.log("redrawing chunk", props.chunkNumber)
-        for (let i = 0; i < PIXELS_PER_CHUNK; i++) {
-          for (let j = 0; j < PIXELS_PER_CHUNK; j++) {
-            let color = pixels[i][j]
-            if (color)
+        if (DEBUG_DRAW) {
+          g.lineStyle(1, 0xff8800)
+        }
+        g.beginFill(0xbbbbbb)
+        g.drawRect(0, 0, 8, 8)
+        g.endFill()
+        g.lineStyle(1 / 16, 0x0)
+        if (pixels.length > 0) {
+          for (let i = 0; i < PIXELS_PER_CHUNK; i++) {
+            for (let j = 0; j < PIXELS_PER_CHUNK; j++) {
+              let color = pixels[i][j]
               g.beginFill(color)
-            else if (DEBUG_DRAW)
-              g.beginFill(0xff00ff)
-        
-            g.lineStyle(0.01, 0x0)
-            g.drawRect(i, j, 1, 1)
-             
-            if (color)
-                g.endFill();
+              g.drawRect(i, j, 1, 1)
+              g.endFill();
+            }
           }
         }
-        if (DEBUG_DRAW) {
-          g.lineStyle(0.1, 0xff8800)
-          g.drawRect(0, 0, 8, 8)
-        }
+
       }}
     />
   );
-};
+});
 
 function createMatrix(side) {
   let matrix = []
